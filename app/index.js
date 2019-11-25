1e0f6490ba2b334a80bf07d77ed0592b869ccaa5
bmikailenko <ben.mikailenko@wsu.edu>
2019-11-25 13:26:53 -0800

    added multiple file input properties

--- app/index.js
+++ app/index.js
@@ -31,51 +31,60 @@ app.post('/upload', function(req,res){
     fs.mkdirSync('./files/' + date);
   }
 
-  // if no uploaded files just exit
+  // if no uploaded files just redirect to home
   if (!req.files || Object.keys(req.files).length === 0) {
-    console.log("quiiit");
     return res.redirect('/');
   }
 
   // Save pdf file 1
-  var pdfFile1 = req.files.pdfFile1;
-  pdfFile1.mv('./files/' + date + '/' + pdfFile1.name, function(err) { if (err) return res.status(500).send(err)});
+  if (req.files.pdfInput[0] != null){
+    var pdfFile1 = req.files.pdfInput[0];
+    pdfFile1.mv('./files/' + date + '/' + pdfFile1.name.replace(/\s+/g, '-'), function(err) { if (err) return res.status(500).send(err)});
+    var downloadLink1 = '<a href=' + ("./files/" + date + "/" + pdfFile1.name.replace(/\s+/g, '-')) + ' download>' + pdfFile1.name + '</a>';
+  }
 
+  // Save pdf file 2
+  if (req.files.pdfInput[1] != null){
+    var pdfFile2 = req.files.pdfInput[1];
+    pdfFile2.mv('./files/' + date + '/' + pdfFile2.name.replace(/\s+/g, '-'), function(err) { if (err) return res.status(500).send(err)});
+    var downloadLink2 = '<a href=' + ("./files/" + date + "/" + pdfFile2.name.replace(/\s+/g, '-')) + ' download>' + pdfFile2.name + '</a>';
+  }
 
-  // // Save pdf file 2
-  // if (req.files.pdfFile2 != null){
-  //   var pdfFile2 = req.files.pdfFile2;
-  //   pdfFile2.mv('./files/' + pdfFile2.name, function(err) { if (err) return res.status(500).send(err)});
-  // }
-  //
-  // // Save pdf file 3
-  // if (req.files.pdfFile3 != null) {
-  //   var pdfFile3 = req.files.pdfFile3;
-  //   pdfFile3.mv('./files/' + pdfFile3.name, function(err) { if (err) return res.status(500).send(err)});
-  // }
-  //
-  // // Save pdf file 4
-  // if (req.files.pdfFile4 != null) {
-  //   var pdfFile4 = req.files.pdfFile4;
-  //   pdfFile4.mv('./files/' + pdfFile4.name, function(err) { if (err) return res.status(500).send(err)});
-  // }
-  //
-  // // Save pdf file 5
-  // if (req.files.pdfFile5 != null) {
-  //   var pdfFile5 = req.files.pdfFile5;
-  //   pdfFile5.mv('./files/' + pdfFile5.name, function(err) { if (err) return res.status(500).send(err)});
-  // }
+  // Save pdf file 3
+  if (req.files.pdfInput[2] != null) {
+    var pdfFile3 = req.files.pdfInput[2];
+    pdfFile3.mv('./files/' + date + '/' + pdfFile3.name.replace(/\s+/g, '-'), function(err) { if (err) return res.status(500).send(err)});
+    var downloadLink3 = '<a href=' + ("./files/" + date + "/" + pdfFile3.name.replace(/\s+/g, '-')) + ' download>' + pdfFile3.name + '</a>';
+  }
 
-  // link to download file
-  var downloadLink = '<a href=' + ("./files/" + date + "/" + pdfFile1.name) + ' download>' + pdfFile1.name + '</a>';
+  // Save pdf file 4
+  if (req.files.pdfInput[3] != null) {
+    var pdfFile4 = req.files.pdfInput[3];
+    pdfFile4.mv('./files/' + date + '/' + pdfFile4.name.replace(/\s+/g, '-'), function(err) { if (err) return res.status(500).send(err)});
+    var downloadLink4 = '<a href=' + ("./files/" + date + "/" + pdfFile4.name.replace(/\s+/g, '-')) + ' download>' + pdfFile4.name + '</a>';
+  }
+
+  // Save pdf file 5
+  if (req.files.pdfInput[4] != null) {
+    var pdfFile5 = req.files.pdfInput[4];
+    pdfFile5.mv('./files/' + date + '/' + pdfFile5.name.replace(/\s+/g, '-'), function(err) { if (err) return res.status(500).send(err)});
+    var downloadLink5 = '<a href=' + ("./files/" + date + "/" + pdfFile5.name.replace(/\s+/g, '-')) + ' download>' + pdfFile5.name + '</a>';
+  }
+
+  var downloadLinkList = [];
+  if (downloadLink1 !== undefined) downloadLinkList.push(downloadLink1 + "<br>");
+  if (downloadLink2 !== undefined) downloadLinkList.push(downloadLink2 + "<br>");
+  if (downloadLink3 !== undefined) downloadLinkList.push(downloadLink3 + "<br>");
+  if (downloadLink4 !== undefined) downloadLinkList.push(downloadLink4 + "<br>");
+  if (downloadLink5 !== undefined) downloadLinkList.push(downloadLink5 + "<br>");
 
   // entry to main page
-  var card = '<div class="card" id=\"' + date +'\" style="width: 18rem; text-align: center; margin: 0 auto;">' +
-  '<div class="card-body">' +
-  '<h5 class="card-title">' +
+  var card = '<div class="card bg-light mb-3" id=\"' + date +'\" style="width: 18rem; text-align: center; margin: 0 auto;">' +
+  '<div class="card-header">' +
   date +
-  '</h5>' +
-  downloadLink +
+  '</div>' +
+  '<div class="card-body">' +
+  downloadLinkList.join("") +
   '</div>' +
   '</div>';
 
@@ -85,6 +94,20 @@ app.post('/upload', function(req,res){
     const $ = jquery(dom.window);
     var content = $('#mainPage').html();
     $('#mainPage').html(content + card);
+
+    // organize
+    var mainPage = $('#mainPage');
+    var dates = mainPage.children('div').get();
+    dates.sort(function(a, b) {
+      var compA = $(a).attr('id').toUpperCase();
+      var compB = $(b).attr('id').toUpperCase();
+      return (compA > compB) ? -1 : (compA < compB) ? 1 : 0;
+    })
+    $.each(dates, function(idx, itm) {
+      mainPage.append(itm);
+    })
+
+    // write
     fs.writeFile(__dirname + '/views/index.html', dom.serialize(), err => {
     });
   });
@@ -134,18 +157,18 @@ app.post('/edit', function(req,res){
   }
 
   // Save pdf file 1
-  var newPdfFile1 = req.files.newPdfFile1;
+  var newPdfFile1 = req.files.pdfInput[0];
   newPdfFile1.mv('./files/' + newDate + '/' + newPdfFile1.name, function(err) { if (err) return res.status(500).send(err)});
 
   // link to download file
   var downloadLink = '<a href=' + ("./files/" + newDate + "/" + newPdfFile1.name) + '>' + newPdfFile1.name + '</a>';
 
   // entry to main page
-  var card = '<div class="card" id=\"' + newDate +'\" style="width: 18rem; text-align: center; margin: 0 auto;">' +
-  '<div class="card-body">' +
-  '<h5 class="card-title">' +
+  var card = '<div class="card bg-light mb-3" id=\"' + newDate +'\" style="width: 18rem; text-align: center; margin: 0 auto;">' +
+  '<div class="card-header">' +
   newDate +
-  '</h5>' +
+  '</div>' +
+  '<div class="card-body">' +
   downloadLink +
   '</div>' +
   '</div>';
